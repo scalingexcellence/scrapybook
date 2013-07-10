f5b58989e38e52ebe6b9e83960c429aef90d0586
root <root@master.(none)>
2013-07-10 20:43:35 +0000

    initial worker spider

--- properties/items.py
+++ properties/items.py
@@ -1,11 +1,17 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 from scrapy.item import Item, Field
 
 class PropertiesItem(Item):
-    # define the fields for your item here like:
-    # name = Field()
-    pass
+    title = Field()
+    price = Field()
+    available = Field()
+    description = Field()
+    image = Field()
+    breadcrumbs = Field()
+    url = Field()
+    website = Field()
+    address = Field()
+    loc = Field()
+    project = Field()
+    spider = Field()
+    server = Field()
+    date = Field()
---
+++ properties/spiders/worker.py
@@ -0,0 +1,46 @@
+from scrapy.contrib.spiders import CrawlSpider
+from properties.items import PropertiesItem
+from scrapy.selector import HtmlXPathSelector
+from datetime import datetime
+import socket, re
+
+class WorkerSpider(CrawlSpider):
+    name = "worker"
+    allowed_domains = ["gumtree.com"]
+
+    def __init__(self, url=None, *a, **kw):
+        super(WorkerSpider, self).__init__(*a, **kw)
+        self.start_urls = [url]
+
+    def parse(self, response):
+        hxs = HtmlXPathSelector(response)
+        item = PropertiesItem()
+
+        def maybe0(v): return v[0] if v and len(v)>0 else ""
+
+        item['title'      ] = maybe0(hxs.select('//*[@id="primary-h1"]/span[1]/text()'     ).extract())
+        item['price'      ] = maybe0(hxs.select('//*[@id="primary-h1"]/span[2]/span/text()').extract())
+        item['available'  ] = maybe0(hxs.select('//*[@id="vip-attributes"]/li[1]/p/text()' ).extract())
+        item['description'] = maybe0(hxs.select('//*[@id="vip-description-text"]/text()'   ).extract())
+        item['image'      ] = maybe0(hxs.select('//*[@id="gallery-item-mid-1"]/a/img/@src' ).extract())
+        item['breadcrumbs'] = hxs.select('//*[@id="breadcrumbs"]/li/a/text()').extract()
+        item['url'        ] = response.url
+        item['website'    ] = "Gumtree"
+        item['address'    ] = maybe0(hxs.select('//*[@class="ad-location"]/text()'         ).extract())
+        item['project'    ] = self.settings.get('BOT_NAME')
+        item['spider'     ] = self.name
+        item['server'     ] = "%s (%s)" % (socket.gethostname(), socket.gethostbyname(socket.gethostname()))
+        item['date'       ] = datetime.now()
+
+        # Price management
+        if not item['price'].strip():
+            self.log('Empty price on %s' % response.url)
+            return
+
+        if item['price'].endswith("pm"):
+            item['price'] = float(re.sub("[^0-9]", "", item['price'      ]))/4.3
+        else:
+            item['price'] = float(re.sub("[^0-9]", "", item['price'      ]))
+
+        return item
+
