0a8de47087883bc17213e683a5951425f54234d6
root <root@master.(none)>
2013-07-10 23:59:48 +0000

    master spider schedules for scrapyd and takes worker hosts from a file (workers.txt by default)

--- properties/spiders/master.py
+++ properties/spiders/master.py
@@ -1,8 +1,10 @@
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import HtmlXPathSelector
-from scrapy import signals
 from scrapy.xlib.pydispatch import dispatcher
+from scrapy import signals
+from random import choice
+import requests, pickle, socket
 
 BATCH_SIZE = 1000
 
@@ -17,10 +19,14 @@ class MasterSpider(CrawlSpider):
         Rule(SgmlLinkExtractor(restrict_xpaths=('//*[@id="pagination"]/ul/li/a',)), callback='parse_links', follow=True),
     )
 
-    def __init__(self, *a, **kw):
+    def __init__(self, workers="workers.txt", *a, **kw):
         super(MasterSpider, self).__init__(*a, **kw)
         dispatcher.connect(self._send_batch, signals.spider_closed)
         self.batch = []
+        try:
+            self.workers = filter(None, [line.strip() for line in open(workers)])
+        except IOError:
+            self.workers = ["localhost"]
 
     def _add_to_batch(self, url):
         self.batch.append(url)
@@ -30,8 +36,20 @@ class MasterSpider(CrawlSpider):
     def _send_batch(self,spider):
         if len(self.batch)==0:
             return
-        print "just created a batch with size %d" % len(self.batch)
+        worker = choice(self.workers)
+        print "just scheduled a batch with size %d -> %s" % (len(self.batch), worker)
+        self._schedule(worker,"properties","worker",pickle.dumps(self.batch),socket.gethostbyname(socket.gethostname()),"properties","properties")
         self.batch=[]
+
+    def _schedule(self,host,project,spider,url,mongohost,db,collection):
+        return requests.post("http://%s:6800/schedule.json" % host , data={
+            "project":project,
+            "spider":spider,
+            "url":url,
+            "mongohost":mongohost,
+            "db":db,
+            "collection":collection
+        })
 
     def parse_links(self, response):
         hxs = HtmlXPathSelector(response)
