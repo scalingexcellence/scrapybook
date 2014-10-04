6d0ed61a9ca0a67e5cb61e1546c3038db8055a8c
Dimitrios Kouzis-Loukas <lookfwd@gmail.com>
2014-10-04 21:27:31 +0200

    adding manual.py

--- ch03/properties/properties/spiders/manual.py
+++ ch03/properties/properties/spiders/manual.py
@@ -9,12 +9,13 @@ import scrapy
 
 
 class BasicSpider(scrapy.Spider):
-    name = 'fast'
-    allowed_domains = ['scrapybook.s3.amazonaws.com']
+    name = "manual"
+    allowed_domains = ["scrapybook.s3.amazonaws.com"]
 
     # Start on the first index page
-    start_urls = ['http://scrapybook.s3.amazonaws.com/'
-                  'properties/index_00000.html']
+    start_urls = (
+        'http://scrapybook.s3.amazonaws.com/properties/index_00000.html',
+    )
 
     def parse(self, response):
         # Get the next index URLs and yield Requests
@@ -22,33 +23,40 @@ class BasicSpider(scrapy.Spider):
         for url in next_selector.extract():
             yield Request(urlparse.urljoin(response.url, url))
 
-        # Iterate through products and create PropertiesItems
-        selectors = response.xpath(
-            '//*[@itemtype="http://schema.org/Product"]')
-        for selector in selectors:
-            yield self.parse_item(selector, response)
+        # Get item URLs and yield Requests
+        item_selector = response.xpath('//*[@itemprop="url"]/@href')
+        for url in item_selector.extract():
+            yield Request(urlparse.urljoin(response.url, url),
+                          callback=self.parse_item)
 
-    def parse_item(self, selector, response):
-        # Create the loader using the selector
-        l = ItemLoader(item=PropertiesItem(), selector=selector)
+    def parse_item(self, response):
+        """ This function parses a property page.
+
+        @url http://scrapybook.s3.amazonaws.com/properties/property_000000.html
+        @returns items 1
+        @scrapes title price description address image_urls
+        @scrapes url project spider server date
+        """
+
+        # Create the loader using the response
+        l = ItemLoader(item=PropertiesItem(), response=response)
 
         # Load fields using XPath expressions
-        l.add_xpath('title', './/*[@itemprop="name"][1]/text()',
+        l.add_xpath('title', '//*[@itemprop="name"][1]/text()',
                     MapCompose(unicode.strip, unicode.title))
         l.add_xpath('price', './/*[@itemprop="price"][1]/text()',
                     MapCompose(lambda i: i.replace(',', ''), float),
                     re='[,.0-9]+')
-        l.add_xpath('description', './/*[@itemprop="description"][1]/text()',
+        l.add_xpath('description', '//*[@itemprop="description"][1]/text()',
                     MapCompose(unicode.strip), Join())
         l.add_xpath('address',
-                    './/*[@itemtype="http://schema.org/Place"][1]/*/text()',
+                    '//*[@itemtype="http://schema.org/Place"][1]/text()',
                     MapCompose(unicode.strip))
-        l.add_xpath('image_urls', './/*[@itemprop="image"][1]/@src',
+        l.add_xpath('image_urls', '//*[@itemprop="image"][1]/@src',
                     MapCompose(lambda i: urlparse.urljoin(response.url, i)))
 
         # Housekeeping fields
-        l.add_xpath('url', './/*[@itemprop="url"][1]/@href',
-                    MapCompose(lambda i: urlparse.urljoin(response.url, i)))
+        l.add_value('url', response.url)
         l.add_value('project', self.settings.get('BOT_NAME'))
         l.add_value('spider', self.name)
         l.add_value('server', (lambda i: i + ' (' + socket.gethostbyname(i) +
