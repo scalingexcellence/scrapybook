9f4f7d664d160deb9c8f43b7cb66bf9948ad334c
Dimitrios Kouzis-Loukas <lookfwd@gmail.com>
2014-10-06 09:11:26 +0100

    moving more spiders to ch05

---
+++ ch05/properties/properties/pipelines.py
@@ -0,0 +1,9 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+class PropertiesPipeline(object):
+    def process_item(self, item, spider):
+        return item
---
+++ ch05/properties/properties/settings.py
@@ -0,0 +1,16 @@
+# Scrapy settings for properties project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'properties'
+
+SPIDER_MODULES = ['properties.spiders']
+NEWSPIDER_MODULE = 'properties.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on
+# the user-agent
+#USER_AGENT = 'properties (+http://www.yourdomain.com)'
