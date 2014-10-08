64f926b132eb5cc96f0bb98ccf862a981d2e7e24
Dimitrios Kouzis-Loukas <lookfwd@gmail.com>
2014-10-08 01:48:04 +0100

    Adding chapter 5

---
+++ ch04/properties/properties/items.py
@@ -0,0 +1,22 @@
+from scrapy.item import Item, Field
+
+
+class PropertiesItem(Item):
+    # Primary fields
+    title = Field()
+    price = Field()
+    description = Field()
+    address = Field()
+    image_urls = Field()
+
+    # Calculated fields
+    images = Field()
+    location = Field()
+    geo_addr = Field()
+
+    # Housekeeping fields
+    url = Field()
+    project = Field()
+    spider = Field()
+    server = Field()
+    date = Field()
